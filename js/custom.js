3fa2464ab859399054c97a2abca4fd197e14d070
Frost <frostfury7@gmail.com>
2020-02-19 16:19:20 +0530

    News Update

--- js/custom.js
+++ js/custom.js
@@ -158,3 +158,13 @@
     });
 
 })(jQuery);
+$('.carousel-main').owlCarousel({
+	items: 1,
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	margin: 10,
+	nav: true,
+	dots: false,
+	navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
+})
